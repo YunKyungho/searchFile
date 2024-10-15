847d6aed9aca53196072d7c99fd2d53a2bba1121
YunKyungho <sghj2020s@gmail.com>
2024-10-15 20:30:42 +0900

    Update database.go

--- pkg/database.go
+++ pkg/database.go
@@ -1,5 +1,20 @@
 package pkg
 
 import (
+	"database/sql"
 	_ "github.com/mattn/go-sqlite3"
+	"log"
 )
+
+type Database struct {
+	handler *sql.DB
+}
+
+func NewDatabase(dbFile string) *Database {
+	handler, err := sql.Open("sqlite3", dbFile)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	db := Database{handler: handler}
+	return &db
+}
