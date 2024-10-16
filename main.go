5c046f249a8379f3e97502198b948d3f73b098ef
YunKyungho <sghj2020s@gmail.com>
2024-10-16 18:32:09 +0900

    Create Database with use sqlite

--- main.go
+++ main.go
@@ -4,38 +4,54 @@ import (
 	"fmt"
 	"os"
 	"path/filepath"
+
+	"github.com/YunKyungho/searchFile/pkg"
 )
 
-// 재귀적으로 모든 파일을 탐색하는 함수
-func buildIndex(root string) error {
-	// WalkDir을 이용해 루트 디렉토리부터 모든 파일과 디렉토리 탐색
+const timeFormat string = "2024-10-19 00:00:00"
+
+// scanDirectory is Browse all files and directories from the root directory using WalkDir
+func scanDirectory(db *pkg.Database, root string) error {
 	return filepath.WalkDir(root, func(path string, d os.DirEntry, err error) error {
 		if err != nil {
-			fmt.Printf("Skipping: %s (Error: %v)\n", path, err)
+			// fmt.Printf("Skipping: %s (Error: %v)\n", path, err)
 			return nil
 		}
 
 		info, err := d.Info()
 		if err != nil {
-			fmt.Printf("Skipping: %s (Error: %v)\n", path, err)
+			// fmt.Printf("Skipping: %s (Error: %v)\n", path, err)
 			return nil
 		}
 
-		size := info.Size()
-		modTime := info.ModTime()
-		isDir := info.IsDir()
+		if info.IsDir() {
+			db.InsertDirectoryInfo(info.Name(), info.ModTime().Format(timeFormat))
+		} else {
+			directory := path[:len(path)-len(info.Name())]
+			diNo := db.SelectDiNo(directory)
+			if diNo != 0 {
+				db.InsertFileInfo(info.Name(), info.ModTime().Format(timeFormat), diNo)
+			}
+		}
 
 		return nil
 	})
 }
 
 func main() {
+	db := pkg.NewDatabase("test.db")
+	defer db.CloseDatabase()
 	// 로컬에 물리적으로 마운트된 파일시스템만 구하는 선행 로직 필요.
 	root := "/"
 
-	if err := buildIndex(root); err != nil {
+	// 디렉토리 탐색 후 경로와 파일명 저장.
+	if err := scanDirectory(db, root); err != nil {
 		fmt.Printf("Error: %v\n", err)
 		return
 	}
 
+	db.CreateIndex()
+
+	// 위 작업 종료 후 파일 CRUD 이벤트 기반으로 무한 루프
+	// + 외부 프로세스가 특정 시그널 보냈을 때 sqlite에서 검색해주는 작업.
 }
--- pkg/database.go
+++ pkg/database.go
@@ -2,19 +2,113 @@ package pkg
 
 import (
 	"database/sql"
-	_ "github.com/mattn/go-sqlite3"
 	"log"
+
+	_ "github.com/mattn/go-sqlite3"
 )
 
 type Database struct {
 	handler *sql.DB
 }
 
+// NewDatabase is Constructor that open DB
 func NewDatabase(dbFile string) *Database {
 	handler, err := sql.Open("sqlite3", dbFile)
 	if err != nil {
 		log.Fatalln(err)
 	}
 	db := Database{handler: handler}
+	db.createTable()
 	return &db
+}
+
+// CloseDatabase is close DB
+func (d Database) CloseDatabase() {
+	d.handler.Close()
+}
+
+// createTable named directory_info and file_info
+func (d Database) createTable() {
+	query := `
+		CREATE TABLE IF NOT EXISTS directory_info (
+			di_no INTEGER PRIMARY KEY AUTOINCREMENT,
+			di_path TEXT NOT NULL,
+			di_modified_date DATE NOT NULL
+			UNIQUE (di_path)
+		);
+	`
+	if _, err := d.handler.Exec(query); err != nil {
+		log.Fatalf("Failed to create directory_info table: %v", err)
+	}
+
+	query = `
+		CREATE TABLE IF NOT EXISTS file_info (
+			fi_no INTEGER PRIMARY KEY AUTOINCREMENT,
+			fi_name TEXT NOT NULL,
+			fi_modified_date DATE NOT NULL,
+			fi_parent INTEGER NOT NULL,
+		    UNIQUE (fi_name, fi_parent),
+			FOREIGN KEY(fi_parent) REFERENCES directory_info(di_no)
+		);
+	`
+	if _, err := d.handler.Exec(query); err != nil {
+		log.Fatalf("Failed to create file_info table: %v", err)
+	}
+}
+
+// CreateIndex of fi_name in file_info
+func (d Database) CreateIndex() {
+	query := `CREATE INDEX IF NOT EXISTS file_info_fi_name_idx ON file_info (fi_name);`
+	if _, err := d.handler.Exec(query); err != nil {
+		log.Fatalf("Failed to create file_info index: %v", err)
+	}
+}
+
+// InsertFileInfo with name, modeDate, parent. parent is foreign key of directory_info
+func (d Database) InsertFileInfo(name string, modDate string, parent int) {
+	query := `INSERT INTO file_info (fi_name, fi_modifed_date, fi_parent) VALUES (?, ?, ?) ON CONFLICT(fi_name, fi_parent) DO NOTHING;`
+	_, err := d.handler.Exec(query, name, modDate, parent)
+	if err != nil {
+		log.Fatalf("Failed to insert file_info: %v", err)
+	}
+}
+
+func (d Database) SelectFileInfo() {
+
+}
+
+func (d Database) UpdateFileInfo() {
+
+}
+
+func (d Database) DeleteFileInfo() {
+
+}
+
+// InsertDirectoryInfo with path, modDate
+func (d Database) InsertDirectoryInfo(path string, modDate string) {
+	query := `INSERT INTO directory_info (di_path, di_modified_date) VALUES (?, ?) ON CONFLICT(di_path) DO NOTHING;`
+	_, err := d.handler.Exec(query, path, modDate)
+	if err != nil {
+		log.Fatalf("Failed to insert derectory_info: %v", err)
+	}
+}
+
+// SelectDiNo with di_path
+func (d Database) SelectDiNo(path string) int {
+	query := `SELECT di_no FROM directory_info WHERE di_path = ?`
+	var diNo int
+	err := d.handler.QueryRow(query, path).Scan(&diNo)
+	if err != sql.ErrNoRows && err != nil {
+		log.Fatalf("Failed to select di_no: %v", err)
+	}
+	return diNo
+}
+
+func (d Database) UpdateDirectoryInfo() {
+
+}
+
+func (d Database) DeleteDirectoryInfo() {
+
 }
